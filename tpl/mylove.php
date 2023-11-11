c30ab1123c52525ee5f8c05bc14a64578935bc05
MaiThiHongNgoc <146064815+MaiThiHongNgoc@users.noreply.github.com>
2023-11-11 12:01:25 +0700

    Create mylove.php

---
+++ tpl/mylove.php
@@ -0,0 +1,266 @@
+<html>
+<link rel="stylesheet" href="lightbox/engine/css/vlightbox.css" type="text/css" />
+<style type="text/css">#vlightbox a#vlb{display:none}</style>
+<link rel="stylesheet" href="lightbox/engine/css/visuallightbox.css" type="text/css" media="screen" />
+<script>
+		$(document).ready(function()
+			{
+			$("#layout").fadeOut(0);
+			$("#layout").fadeIn(500);
+			});
+</script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js" type="text/javascript"></script>
+<script src="js/jquery.lettering-0.6.min.js" type="text/javascript"></script>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js"></script>
+		
+<script type="text/javascript" src="js/script.js"></script>
+
+<script>
+	$(document).ready(function() {
+	    $(".title").lettering();
+	  });
+</script>
+<script>alert('Khi vào web thì em nhớ bật đèn nhé');</script>
+<style>
+#switch {
+	position:absolute;
+	bottom:530px;
+	right:150px;
+    margin-right: 20px;
+	background:url(img/light-switch.png) -80px 0 no-repeat;
+	width:80px;
+	height:120px;
+	cursor:pointer;
+	z-index:100;
+}
+
+
+#light-bulb {
+    margin-top: 15px;
+	left:10%;
+	width:150px;
+	height:150px;
+	background:url(img/lightbulb.png) -150px 0 no-repeat;
+	cursor:move;
+	z-index:800;
+}
+#light-bulb2 {
+	width:150px;
+	height:150px;
+	display:none;
+	background:url(img/lightbulb.png) 0 0 no-repeat;
+}
+
+#logo {
+	position:absolute;
+	top:40%;
+	left:20%;
+	width:450px;
+	height:150px;
+	cursor:move;
+	z-index:700;
+}
+#logosh {
+m
+	position:absolute;
+	display:none;
+	text-shadow:1px 1px black;
+
+}
+
+h3{ 
+margin-top: -100px;
+    text-align: center;
+font-family: "Museo";
+font-size: 25px;
+text-transform: uppercase;
+color: #fff;
+text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #ff00de, 0 0 70px #ff00de, 0 0 80px #ff00de, 0 0 100px #ff00de, 0 0 150px #ff00de;
+}
+
+</style>
+
+<body>
+ <object data="http://teenphuyen.com/valentine/dep.swf" type="application/x-shockwave-flash" style="width: 100%; height: 100%;position: absolute;left: 0; margin-top: -230px;"><param name="movie" value="dep.swf"><param name="wmode" value="transparent"></object>
+ <object data="http://teenphuyen.com/valentine/bay.swf" type="application/x-shockwave-flash" style="width: 100%; height: 100%;position: absolute;left: 0;"><param name="movie" value="dep.swf"><param name="wmode" value="transparent"></object>
+
+<div id="switch"></div>
+		<div id="light-bulb" class="off"><div id="light-bulb2"></div></div>
+    <div id="logo"></div>
+		<div id="logosh">
+<style>
+</style>
+<body>
+<div class="layout" id="layout">
+	<div><h3>Chúc Vk Yêu 8/3 Vui Vẻ</h3></div>
+
+	<div class="navbar">
+	<b><script type="text/javascript">
+    <!-- Begin
+    function toSpans(span) {
+      var str=span.firstChild.data;
+      var a=str.length;
+      span.removeChild(span.firstChild);
+      for(var i=0; i<a; i++) {
+        var theSpan=document.createElement("SPAN");
+        theSpan.appendChild(document.createTextNode(str.charAt(i)));
+        span.appendChild(theSpan);
+      }
+    }
+
+    function RainbowSpan(span, hue, deg, brt, spd, hspd) {
+        this.deg=(deg==null?360:Math.abs(deg));
+        this.hue=(hue==null?0:Math.abs(hue)%360);
+        this.hspd=(hspd==null?3:Math.abs(hspd)%360);
+        this.length=span.firstChild.data.length;
+        this.span=span;
+        this.speed=(spd==null?50:Math.abs(spd));
+        this.hInc=this.deg/this.length;
+        this.brt=(brt==null?255:Math.abs(brt)%256);
+        this.timer=null;
+        toSpans(span);
+        this.moveRainbow();
+    }
+    RainbowSpan.prototype.moveRainbow = function() {
+      if(this.hue>359) this.hue-=360;
+      var color;
+      var b=this.brt;
+      var a=this.length;
+      var h=this.hue;
+
+      for(var i=0; i<a; i++) {
+
+        if(h>359) h-=360;
+
+        if(h<60) { color=Math.floor(((h)/60)*b); red=b;grn=color;blu=0; }
+        else if(h<120) { color=Math.floor(((h-60)/60)*b); red=b-color;grn=b;blu=0; }
+        else if(h<180) { color=Math.floor(((h-120)/60)*b); red=0;grn=b;blu=color; }
+        else if(h<240) { color=Math.floor(((h-180)/60)*b); red=0;grn=b-color;blu=b; }
+        else if(h<300) { color=Math.floor(((h-240)/60)*b); red=color;grn=0;blu=b; }
+        else { color=Math.floor(((h-300)/60)*b); red=b;grn=0;blu=b-color; }
+
+        h+=this.hInc;
+
+        this.span.childNodes[i].style.color="rgb("+red+", "+grn+", "+blu+")";
+      }
+      this.hue+=this.hspd;
+    }
+    </script>
+    <h2 style="margin-top: 1px;" id="r1">Edit By : Nguyễn Khắc Cường</h2>
+    <script type="text/javascript">
+    var r1=document.getElementById("r1"); //get span to apply rainbow
+    var myRainbowSpan=new RainbowSpan(r1, 0, 360, 255, 50, 18); //apply static rainbow effect
+    myRainbowSpan.timer=window.setInterval("myRainbowSpan.moveRainbow()", myRainbowSpan.speed);
+    </script></b></a>
+    	<div style="float:right;">
+		
+         <div style="margin-right: 15px;" class="navbutton"><a target="_blank" href="http://khaccuong.esy.es">About me</a></div>
+         <div style="margin-right: 15px;" class="navbutton"><a target="_blank" href="https://www.facebook.com/Kuong.Uong.Buong">Facebook</a></div>
+         <div class="navbutton"><a><marquee>Chào mừng bạn đã đến với webiste do Cường gửi tặng Yến.Chúc các bạn vui vẻ.</marquee></a></div>
+
+        </div>
+    </div>
+    
+    
+  
+    <div class="content">
+    	
+        <div class="c2">
+		
+        
+        <div style="float:left;width:400px;margin-top: 35px;" align="center">
+				
+				<a class="vlightbox" href="lightbox/data/images/6.jpg" title="IMG_1998"><img style="background-size: 100% 100%; width: 162px; height: 127px;" src="lightbox/data/thumbnails/5.jpg" alt="Vk Yến"/></a>
+				<a class="vlightbox" href="lightbox/data/images/7.jpg" title="IMG_1998"><img style="background-size: 100% 100%; width: 162px; height: 127px;" src="lightbox/data/thumbnails/7.jpg" alt="Vk Yến"/></a>
+				<a class="vlightbox" href="lightbox/data/images/1.jpg" title="IMG_1998"><img style="background-size: 100% 100%; width: 162px; height: 127px;" src="lightbox/data/thumbnails/1.jpg" alt="Bi ÂĐ"/></a>
+				<a class="vlightbox" href="lightbox/data/images/2.jpg" title="IMG_1998"><img style="background-size: 100% 100%; width: 162px; height: 127px;" src="lightbox/data/thumbnails/2.jpg" alt="Vk Yến"/></a>
+				<a class="vlightbox" href="lightbox/data/images/6.jpg" title="IMG_1998"><img style="background-size: 100% 100%; width: 162px; height: 127px;" src="lightbox/data/thumbnails/6.jpg" alt="Vk Yến"/></a>
+				<a class="vlightbox" href="lightbox/data/images/3.jpg" title="IMG_1998"><img style="background-size: 100% 100%; width: 162px; height: 127px;" src="lightbox/data/thumbnails/3.jpg" alt="Vk Yến"/></a>
+				<a class="vlightbox" href="lightbox/data/images/4.jpg" title="IMG_1998"><img style="background-size: 100% 100%; width: 162px; height: 127px;" src="lightbox/data/thumbnails/4.jpg" alt="Vk Yến"/></a>
+			<br /><br /><br /><br /><br /><br />
+            	<script src="lightbox/engine/js/visuallightbox.js" type="text/javascript"></script>
+			</div>
+
+
+
+
+	<div style="float:left;width:428px;height:340px; padding: 30px">
+
+
+			<?php
+			date_default_timezone_set('Asia/Jakarta');
+			
+			$firsttime= mktime(09,00,00,10,4,2013);
+			$today=time();
+			$ngayquen=($today-$firsttime)/(3600 * 24);
+			$gioquen=($ngayquen-intval($ngayquen))*24;
+			$phutquen=($gioquen-intval($gioquen))*60;
+			$giayquen=($phutquen-intval($phutquen))*60;
+			
+			$ngayquen=intval($ngayquen);
+			$gioquen=intval($gioquen);
+			$phutquen=intval($phutquen);
+			$giayquen=intval($giayquen);
+			
+			$firsttimex= mktime(09,00,00,10,4,2013);
+			$ngaytotinh=($today-$firsttimex)/(3600 * 24);
+			$giototinh=($ngaytotinh-intval($ngaytotinh))*24;
+			$phuttotinh=($giototinh-intval($giototinh))*60;
+			$giaytotinh=($phuttotinh-intval($phuttotinh))*60;
+			
+			$ngaytotinh=intval($ngaytotinh);
+			$giototinh=intval($giototinh);
+			$phuttotinh=intval($phuttotinh);
+			$giaytotinh=intval($giaytotinh);
+			
+			
+			?>
+            
+            
+            
+        
+            
+			<h2 style="text-align:center">Ck Đẹp Trai Gửi Tặng Vk Xinh Gái</h2>
+            <p align="center"><embed src="http://img194.imageshack.us/img194/7485/102.swf" width="100%" height="420" wmode="transparent" flashvars="go=Chào Vk Yến ! Có những điều thật giản dị và dễ hiểu nhưng đôi khi trong cuộc sống,
+Chúng ta lại được biết nhau tình cờ qua mạng. Trải qua 1 thời gian thì anh đã không còn ngại ngùng khi nói chiện với em :) , tuy chưa biết và hiểu nhau nhiều nhưng có lẽ anh đã hiểu bản tính thân thiện và hòa đồng của em.
+Vì thế mà hôm nay mới có dịp làm cái này để tặng em (Vk Yến) làm kỷ niệm.		
+....Gởi Tặng Vk (..ký tên : Khắc Cường)) ">  
+       </p>
+<br>Đã <b><?php echo $ngayquen;?></b> ngày, <b><?php echo $gioquen;?></b> giờ, <b><?php echo $phutquen;?></b> phút 
+			kể từ lần đầu mình nói chuyện với nhau <img src="http://l.yimg.com/us.yimg.com/i/mesg/emoticons7/105.gif" alt="8->"/><br/>
+			
+				<h2 style="text-align:center"><img src="http://l.yimg.com/us.yimg.com/i/mesg/emoticons7/8.gif" alt=":X"/> CHÚC Vk Yêu MÃI MÃI THÀNH CÔNG  <img src="http://l.yimg.com/us.yimg.com/i/mesg/emoticons7/8.gif" alt=":X"/></h2>
+			<script type='text/javascript' src='swfobject.js'></script>
+			<div id='mediaspace' align="center" ></div>
+			<script type='text/javascript'>
+			  var so = new SWFObject('player.swf','ply','300','24','9','#000000');
+			  so.addParam('allowfullscreen','true');
+			  so.addParam('allowscriptaccess','always');
+			  so.addParam('wmode','opaque');
+			  so.addVariable('file','loveparadise.mp3');
+			  so.addVariable('autostart','1');
+			  so.addVariable('frontcolor','660066');
+			  so.addVariable('lightcolor','660066');
+			  so.addVariable('screencolor','660066');
+			  so.addVariable('repeat','always')
+			  so.write('mediaspace');
+			</script>
+			
+
+
+
+
+
+
+
+			</p>
+  </div>
+<div style="clear:both"></div>
+        </div>
+        <div class="c3"></div>
+        </div>
+</div>
+</div>
+</body>
+</html>
