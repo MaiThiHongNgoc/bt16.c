5d675b6c3bd03bcdfea57127c7859049f2324b66
MaiThiHongNgoc <146064815+MaiThiHongNgoc@users.noreply.github.com>
2023-11-11 12:01:35 +0700

    Create 404.php

---
+++ tpl/404.php
@@ -0,0 +1,31 @@
+</head>
+<body>
+<div class="layout">
+	<div>
+		<a href="index.html" title="Return home"><img src="img/logo.png" alt="www.SoiGia.net" border="0" /></a>
+    </div>
+	<div class="navbar">
+    	<div style="float:right;">
+        
+         <div class="navbutton"><a href="index.html">Home</a></div>    
+         <div class="navbutton"><a href="enter.html">Enter</a></div>
+         <div class="navbutton"><a href="mylove.html">My Love</a></div>
+
+        </div>
+    </div>
+    
+    <div class="content">
+    	<div class="c1"></div>
+        <div class="c2">
+        	<div class="fullside">
+			<h1>404 Error !!!</h1>
+			Xin lỗi nhưng trang này không tồn tại hoặc đã bị xoá !
+			</div>
+
+        <div style="clear:both"></div>
+        </div>
+        <div class="c3"></div>
+    </div>
+</div>
+</body>
+</html>
